3f0a4f45a1fc00d2a3f0ad98dd7a188175154a50
daemonad <daemonad@protonmail.com>
2018-12-22 23:31:42 +0530

    Fixed shit

--- commands/welcomemessage.js
+++ commands/welcomemessage.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const handleError = require('../handlers/errorHandler.js')
-exports.run = (bot, message, [content]) => {
+exports.run = (bot, message, [...content]) => {
   if (!content) return handleError.run(bot, message, `Empty message`, `No welcome message provided.`)
   const filepath = path.normalize(`${__dirname}/../data/${message.guild.id}.json`)
   let settings = JSON.parse(fs.readFileSync(filepath, 'utf-8'))
